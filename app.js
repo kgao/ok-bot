182d2492afca3433f1141e05b1db15e6c30ccf45
kgao <kevingao@microsoft.com>
2016-08-23 19:27:43 -0400

    update app id

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-
+var conf = require('./config');
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID || conf.microsoft.app.id,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD || conf.microsoft.app.password
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
